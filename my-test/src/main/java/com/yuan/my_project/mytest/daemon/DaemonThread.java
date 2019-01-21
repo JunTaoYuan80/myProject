2dfc832edbdf47905e14cbe86a2839814061f2e3
yuanjuntao <yuanjuntao@01zhuanche.com>
2019-01-21 21:06:27 +0800

    daemon thread

--- my-test/src/main/java/com/yuan/my_project/mytest/daemon/DaemonThread.java
+++ my-test/src/main/java/com/yuan/my_project/mytest/daemon/DaemonThread.java
@@ -2,6 +2,11 @@ package com.yuan.my_project.mytest.daemon;
 
 /**
  * daemon线程，当主进程消亡时，守护线程跟着消亡；non-daemon线程，当主进程消亡时，守护线程继续存在
+ * <p>
+ * Daemon thread is a low priority thread (in context of JVM) that runs in background to perform tasks such as garbage collection (gc) etc.,
+ * they do not prevent the JVM from exiting (even if the daemon thread itself is running) when all the user threads (non-daemon threads) finish their execution.
+ * JVM terminates itself when all user threads (non-daemon threads) finish their execution, JVM does not care whether Daemon thread is running or not,
+ * if JVM finds running daemon thread (upon completion of user threads), it terminates the thread and after that shutdown itself.
  *
  * @author yuanjuntao
  * @date 2019/1/21 20:53
